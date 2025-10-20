d7d494ba64f77adaa711e7e79d70efa084a4af72
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2025-10-20 16:43:20 +0200

    ci: added deployment script and configured sphinx

--- contracts/scripts/Deploy.s.sol
+++ contracts/scripts/Deploy.s.sol
@@ -1,99 +1,77 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.16;
-//
-// import {Script} from "forge-std/Script.sol";
-// import {ITypeface} from "lib/typeface/contracts/interfaces/ITypeface.sol";
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {DefifaDelegate} from "../DefifaDelegate.sol";
-// import {DefifaDeployer} from "../DefifaDeployer.sol";
-// import {DefifaGovernor} from "../DefifaGovernor.sol";
-// import {DefifaProjectOwner} from "../DefifaProjectOwner.sol";
-// import {DefifaTokenUriResolver} from "../DefifaTokenUriResolver.sol";
-//
-// contract DeployMainnet is Script {
-//     // V3_1 mainnet controller.
-//     IJBController3_1 _controller = IJBController3_1(0x97a5b9D9F0F7cD676B69f584F29048D0Ef4BB59b);
-//
-//     IJBDelegatesRegistry _delegateRegistry = IJBDelegatesRegistry(0x7A53cAA1dC4d752CAD283d039501c0Ee45719FaC);
-//     ITypeface _typeface = ITypeface(0xA77b7D93E79f1E6B4f77FaB29d9ef85733A3D44A);
-//
-//     uint256 _defifaProjectId = 369;
-//     uint256 _baseProtocolProjectId = 1;
-//
-//     IERC20 _defifaToken = IERC20(address(_controller.tokenStore().tokenOf(_defifaProjectId)));
-//     IERC20 _baseProtocolToken = IERC20(address(_controller.tokenStore().tokenOf(_baseProtocolProjectId)));
-//
-//     uint256 _blockTime = 12;
-//
-//     function run() external {
-//         vm.startBroadcast();
-//
-//         // Deploy the codeOrigin for the delegate.
-//         DefifaDelegate _defifaDelegateCodeOrigin = new DefifaDelegate(_defifaToken, _baseProtocolToken);
-//
-//         // Deploy the token uri resolver.
-//         DefifaTokenUriResolver _defifaTokenUriResolver = new DefifaTokenUriResolver(_typeface);
-//
-//         // Deploy the governor.
-//         DefifaGovernor _defifaGovernor = new DefifaGovernor(_controller, _blockTime);
-//
-//         // Deploy the deployer.
-//         DefifaDeployer _defifaDeployer = new DefifaDeployer(
-//           address(_defifaDelegateCodeOrigin),
-//           _defifaTokenUriResolver,
-//           _defifaGovernor,
-//           _controller,
-//           _delegateRegistry,
-//           _defifaProjectId,
-//           _baseProtocolProjectId
-//         );
-//
-//         new DefifaProjectOwner(IJBOperatable(address(_controller)).operatorStore(), _controller.projects(), _defifaDeployer);
-//
-//         _defifaGovernor.transferOwnership(address(_defifaDeployer));
-//     }
-// }
-//
-// contract DeployGoerli is Script {
-//     // V3_1 goerli controller.
-//     IJBController3_1 _controller = IJBController3_1(0x1d260DE91233e650F136Bf35f8A4ea1F2b68aDB6);
-//
-//     IJBDelegatesRegistry _delegateRegistry = IJBDelegatesRegistry(0xCe3Ebe8A7339D1f7703bAF363d26cD2b15D23C23);
-//     ITypeface _typeface = ITypeface(0x8Df17136B20DA6D1E23dB2DCdA8D20Aa4ebDcda7);
-//
-//     uint256 _defifaProjectId = 1068;
-//     uint256 _baseProtocolProjectId = 1;
-//
-//     IERC20 _defifaToken = IERC20(address(_controller.tokenStore().tokenOf(_defifaProjectId)));
-//     IERC20 _baseProtocolToken = IERC20(address(_controller.tokenStore().tokenOf(_baseProtocolProjectId)));
-//
-//     uint256 _blockTime = 12;
-//
-//     function run() external {
-//         vm.startBroadcast();
-//
-//         // Deploy the codeOrigin for the delegate
-//         DefifaDelegate _defifaDelegateCodeOrigin = new DefifaDelegate(_defifaToken, _baseProtocolToken);
-//
-//         // Deploy the token uri resolver.
-//         DefifaTokenUriResolver _defifaTokenUriResolver = new DefifaTokenUriResolver(_typeface);
-//
-//         // Deploy the governor.
-//         DefifaGovernor _defifaGovernor = new DefifaGovernor(_controller, _blockTime);
-//
-//         // Deploy the deployer.
-//         DefifaDeployer _defifaDeployer = new DefifaDeployer(
-//           address(_defifaDelegateCodeOrigin),
-//           _defifaTokenUriResolver,
-//           _defifaGovernor,
-//           _controller,
-//           _delegateRegistry,
-//           _defifaProjectId,
-//           _baseProtocolProjectId
-//         );
-//
-//         _defifaGovernor.transferOwnership(address(_defifaDeployer));
-//
-//         new DefifaProjectOwner(IJBOperatable(address(_controller)).operatorStore(), _controller.projects(), _defifaDeployer);
-//     }
-// }
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {ITypeface} from "lib/typeface/contracts/interfaces/ITypeface.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {DefifaDelegate} from "../DefifaDelegate.sol";
+import {DefifaDeployer} from "../DefifaDeployer.sol";
+import {DefifaGovernor} from "../DefifaGovernor.sol";
+import {DefifaProjectOwner} from "../DefifaProjectOwner.sol";
+import {DefifaTokenUriResolver} from "../DefifaTokenUriResolver.sol";
+import {Sphinx} from "@sphinx-labs/contracts/SphinxPlugin.sol";
+
+import "@bananapus/core-v5/script/helpers/CoreDeploymentLib.sol";
+import "@bananapus/address-registry-v5/script/helpers/AddressRegistryDeploymentLib.sol";
+
+contract DeployMainnet is Script, Sphinx {
+    /// @notice tracks the deployment of the core contracts for the chain we are deploying to.
+    CoreDeployment core;
+    /// @notice tracks the deployment of the address registry for the chain we are deploying to.
+    AddressRegistryDeployment registry;
+
+    uint256 _defifaProjectId = 369;
+    uint256 _baseProtocolProjectId = 1;
+
+    ITypeface _typeface = ITypeface(0xA77b7D93E79f1E6B4f77FaB29d9ef85733A3D44A);
+    
+    IERC20 defifaToken;
+    IERC20 baseProtocolToken;
+
+    uint256 _blockTime = 12;
+
+    function configureSphinx() public override {
+        sphinxConfig.projectName = "defifa-v5";
+        // TODO: We need to patch the `blockTime` logic in the governor before L2s will function as expected.
+        sphinxConfig.mainnets = ["ethereum"];
+        sphinxConfig.testnets = ["ethereum_sepolia"];
+    }
+
+    function run() external {
+         // Get the deployment addresses for the nana CORE for this chain.
+        // We want to do this outside of the `sphinx` modifier.
+        core = CoreDeploymentLib.getDeployment(
+            vm.envOr("NANA_CORE_DEPLOYMENT_PATH", string("node_modules/@bananapus/core-v5/deployments/"))
+        );
+
+        registry = AddressRegistryDeploymentLib.getDeployment(
+            vm.envOr(
+                "NANA_ADDRESS_REGISTRY_DEPLOYMENT_PATH",
+                string("node_modules/@bananapus/address-registry-v5/deployments/")
+            )
+        );
+
+        defifaToken = IERC20(address(core.tokens.tokenOf(_defifaProjectId)));
+        baseProtocolToken = IERC20(address(core.tokens.tokenOf(_baseProtocolProjectId)));
+
+        // Perform the deployment transactions.
+        deploy();
+    }
+
+    function deploy() public sphinx {
+        DefifaDelegate delegate = new DefifaDelegate(core.directory, defifaToken, baseProtocolToken);
+        DefifaTokenUriResolver tokenUriResolver = new DefifaTokenUriResolver(_typeface);
+        DefifaGovernor governor = new DefifaGovernor(core.controller, _blockTime);
+        DefifaDeployer deployer = new DefifaDeployer(
+            address(delegate),
+            tokenUriResolver,
+            governor,
+            core.controller,
+            registry.registry,
+            _defifaProjectId,
+            _baseProtocolProjectId
+        );
+
+        governor.transferOwnership(address(deployer));
+    }
+}
